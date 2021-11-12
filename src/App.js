19b476c71616debfa5aebcca4079562d7af4ab3a
ptlnik13 <ptlnik13@gmail.com>
2021-11-12 09:39:32 -0500

    106. Storing user data in Firestore and passing to out application state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {Route, Switch} from "react-router-dom";
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 import './App.css';
 
@@ -21,8 +21,22 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    // console.log(snapShot.data());
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                })
+            } else {
+                this.setState({currentUser: userAuth});
+            }
         })
     }
 
