008598f1c6d1038528cb7b2fd84c6c2c97369d59
ptlnik13 <ptlnik13@gmail.com>
2021-12-06 16:31:34 -0500

    144. Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import {logger} from "redux-logger/src";
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from './root-reducer';
+import {persistStore} from "redux-persist";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {store, persistor};
