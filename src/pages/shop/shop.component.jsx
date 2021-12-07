45ad4dd3535a2f0b50ca5787134f9104ea094269
ptlnik13 <ptlnik13@gmail.com>
2021-12-07 09:53:26 -0500

    147. Collection Overview Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,12 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+const ShopPage = () => (
+    <div className='shop-page'>
+        <CollectionsOverview/>
+    </div>
+);
 
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({id, ...otherCollectionProps}) => (
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
-            </div>
-        )
-    };
-}
 
 export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -8,6 +8,3 @@ const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {store, persistor};
